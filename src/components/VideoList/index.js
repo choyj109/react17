e35702b8654deec3eb315f545d1beb3f0cbd3b62
choyj109 <choyj109@gmail.com>
2022-07-19 14:56:45 +0900

    20220719

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import VideoItem from "../VideoItem/index";
+import { useDispatch, useSelector } from "react-redux";
+import { getVideoList } from "../../store/video/videoSlice";
+
 const VideoList = () => {
+  const dispatch = useDispatch();
+  const videoData = useSelector((state) => state.video.data);
+  useEffect(() => {
+    const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=AIzaSyAhFLFwT3drY584SzxWQj9mIqworRr5Cl0`;
+    dispatch(getVideoList(url));
+  }, []);
   return (
-    <ul className="videoList VideoGrid">
-      <VideoItem />
+    <ul className="videoList videoGrid">
+      {videoData.map((item, idx) => {
+        return (
+          <VideoItem
+            key={item.snippet.thumbnails.default.url}
+            item={item.snippet}
+          />
+        );
+      })}
     </ul>
   );
 };
--- src/store/video/videoSlice.js
+++ src/store/video/videoSlice.js
@@ -1,14 +1,27 @@
-import axios from 'axios';
-import { createSlice ,createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const getPopularVideoList = createAsyncThunk(
-   
-)
+export const getVideoList = createAsyncThunk("GET_VIDEO_LIST", async (url) => {
+  try {
+    const res = await axios.get(url);
+    return res.data.items;
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+const videoSlice = createSlice({
+  name: "video",
+  initialState: {
+    data: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getVideoList.fulfilled, (state, action) => {
+      console.log("액션페이로드", action.payload);
+      state.data = action.payload;
+    });
+  },
+});
 
-const videoSlice =  createSlice({
-
-})
-
-
-export default videoSlice.reducer
+export default videoSlice.reducer;
